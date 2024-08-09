146d1db3a0b5a5b920c7f5e79157303713799204
emanning686 <emanning1030@gmail.com>
2024-08-08 23:27:25 -0400

    page setup and readme start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { useEffect } from "react";
+
+async function createLocomotiveScroll() {
+  const LocomotiveScroll = (await import("locomotive-scroll")).default;
+  const locomotiveScroll = new LocomotiveScroll();
+}
+
 export default function Home() {
-  return <main></main>;
+  useEffect(() => {
+    createLocomotiveScroll();
+  }, []);
+  return (
+    <main>
+      <div className="flex flex-col gap-4">
+        <div className="h-screen w-full bg-orange-400"></div>
+        <div className="h-screen w-full bg-orange-400"></div>
+        <div className="h-screen w-full bg-orange-400"></div>
+      </div>
+    </main>
+  );
 }
