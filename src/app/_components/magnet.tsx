abb077bade234200f29e1f6e7e04378e100b0f89
emanning686 <emanning1030@gmail.com>
2024-08-16 22:06:25 -0400

    bugfix

--- src/app/_components/magnet.tsx
+++ src/app/_components/magnet.tsx
@@ -15,21 +15,21 @@ export default function Magnet({ src }: { src: string }) {
       ease: "elastic.out(1, 0.5)",
     });
 
-    if (magnetic.current) {
-      magnetic.current.addEventListener("mousemove", (e) => {
-        const { clientX, clientY } = e;
-        const { height, width, left, top } =
-          magnetic.current.getBoundingClientRect();
-        const x = clientX - (left + width / 2);
-        const y = clientY - (top + height / 2);
-        xTo(x);
-        yTo(y);
-      });
-      magnetic.current.addEventListener("mouseleave", (e) => {
-        xTo(0);
-        yTo(0);
-      });
-    }
+    if (!magnetic.current) return;
+    magnetic.current.addEventListener("mousemove", (e) => {
+      if (!magnetic.current) return;
+      const { clientX, clientY } = e;
+      const { height, width, left, top } =
+        magnetic.current.getBoundingClientRect();
+      const x = clientX - (left + width / 2);
+      const y = clientY - (top + height / 2);
+      xTo(x);
+      yTo(y);
+    });
+    magnetic.current.addEventListener("mouseleave", (e) => {
+      xTo(0);
+      yTo(0);
+    });
   }, []);
 
   return React.cloneElement(
